b5e5ff419fcddb67318be0d0766428b8defac897
Sulu Edward Julianto <93524287+SuluJulianto@users.noreply.github.com>
2024-01-31 15:17:03 +0700

    Update password-validator.c

--- c/password-validator.c
+++ c/password-validator.c
@@ -5,21 +5,30 @@
 
 struct PasswordValidator {
     char password[MAX_PASSWORD_LENGTH];
-    int strength;
+    int length_strength;
+    int uppercase_strength;
+    int lowercase_strength;
+    int numbers_strength;
+    int symbols_strength;
 };
 
 void initialize(struct PasswordValidator *validator, const char *password) {
     strncpy(validator->password, password, sizeof(validator->password));
     validator->password[sizeof(validator->password) - 1] = '\0';  // Null-terminate the string
-    validator->strength = 0;
+    validator->length_strength = 0;
+    validator->uppercase_strength = 0;
+    validator->lowercase_strength = 0;
+    validator->numbers_strength = 0;
+    validator->symbols_strength = 0;
 }
 
 int validateLength(struct PasswordValidator *validator) {
     int length = strlen(validator->password);
     if (length >= 5 && length <= 9) {
+        validator->length_strength = 1;
         return 1;
     } else if (length >= 10) {
-        validator->strength += 2;
+        validator->length_strength = 2;
         return 1;
     } else {
         return 0;
@@ -27,29 +36,29 @@ int validateLength(struct PasswordValidator *validator) {
 }
 
 int validateSpecialChars(struct PasswordValidator *validator, const char *specialChars) {
-    if (strpbrk(validator->password, specialChars) != NULL) {
-        validator->strength += 1;
-        return 1;
-    } else {
-        return 0;
+    int count = 0;
+    for (const char *ch = validator->password; *ch; ++ch) {
+        if (strchr(specialChars, *ch) != NULL) {
+            ++count;
+        }
     }
+    validator->symbols_strength = count;
+    return count > 0;
 }
 
 int validateNumbers(struct PasswordValidator *validator, const char *numbersChars) {
-    if (strpbrk(validator->password, numbersChars) != NULL) {
-        validator->strength += 1;
-        return 1;
-    } else {
-        return 0;
+    int count = 0;
+    for (const char *ch = validator->password; *ch; ++ch) {
+        if (strchr(numbersChars, *ch) != NULL) {
+            ++count;
+        }
     }
+    validator->numbers_strength = count;
+    return count > 0;
 }
 
-int validateSpaces(struct PasswordValidator *validator) {
-    if (strchr(validator->password, ' ') == NULL) {
-        return 1;
-    } else {
-        return 0;
-    }
+int validateSpaces(const struct PasswordValidator *validator) {
+    return strchr(validator->password, ' ') == NULL;
 }
 
 void displayPassword(const struct PasswordValidator *validator) {
@@ -58,16 +67,33 @@ void displayPassword(const struct PasswordValidator *validator) {
 
 void summary(struct PasswordValidator *validator, const char *specialChars, const char *numbersChars) {
     displayPassword(validator);
+
     if (!validateSpaces(validator)) {
         printf("YOUR PASSWORD IS INVALID [CONTAINS SPACES]\n");
-    } else {
-        printf(validateNumbers(validator, numbersChars) ? "Password contains numbers.\n" : "Password does not contain any numbers.\n");
-        printf(validateSpecialChars(validator, specialChars) ? "Password contains at least one special character.\n" : "Password does not contain any special character.\n");
-        if (validateLength(validator)) {
-            printf("Password has a normal or strong length.\n");
-        }
-        printf("SUMMARY: YOUR PASSWORD STRENGTH IS %d / 100%%\n", validator->strength * 25);
+        return;
     }
+
+    if (validateLength(validator)) {
+        printf("Password length is sufficient.\n");
+    } else {
+        printf("Password length is insufficient.\n");
+    }
+
+    if (validateNumbers(validator, numbersChars)) {
+        printf("Password contains numbers.\n");
+    } else {
+        printf("Password does not contain any numbers.\n");
+    }
+
+    if (validateSpecialChars(validator, specialChars)) {
+        printf("Password contains special characters.\n");
+    } else {
+        printf("Password does not contain any special characters.\n");
+    }
+
+    int total_strength = validator->length_strength + validator->numbers_strength +
+                         validator->symbols_strength;
+    printf("SUMMARY: YOUR PASSWORD STRENGTH IS %d / 100%%\n", total_strength * 25);
 }
 
 int main() {
